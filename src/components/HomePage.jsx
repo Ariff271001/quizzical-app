4c693b7522c01e93888987c5267ccd34e1840005
Ariff Haikal <76397403+Ariff271001@users.noreply.github.com>
2026-04-17 20:19:39 +0800

    Added HomePage Component

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+export function HomePage() {
+  return (
+    <>
+      <div class="blob-top-right"></div>
+      <div class="blob-bottom-left"></div>
+
+      <div class="content">
+        <h1>Quizzical</h1>
+        <p>Some description if needed</p>
+        <button>Start quiz</button>
+      </div>
+    </>
+  );
+}
