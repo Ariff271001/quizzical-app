968a959089313045bfd6583a658e7734ea10eb4a
Ariff Haikal <76397403+Ariff271001@users.noreply.github.com>
2026-04-19 22:36:06 +0800

    Completed the app

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [isHomePage, setIsHomePage] = useState(true);
+  const [isChecked, setIsChecked] = useState(false);
   const [selectedAnswers, setSelectedAnswers] = useState({});
   const [quizzes, setQuizzes] = useState([]);
+  const [correctAnswers,setCorrectAnswers] = useState(0);
 
   useEffect(() => {
     const controller = new AbortController();
@@ -25,6 +27,13 @@ function App() {
     return () => controller.abort(); // cancels the first call when effect re-runs
   }, []);
 
+  async function fetchData(){
+    const res = await fetch("https://opentdb.com/api.php?amount=5&category=9&type=multiple");
+    const data = await res.json();
+    const result = data.results;
+    setQuizzes(result)
+  }
+
   function start() {
     setIsHomePage((prev) => !prev);
     console.log(isHomePage);
@@ -33,20 +42,43 @@ function App() {
   function handleSelect(questionIndex, answer) {
     setSelectedAnswers((prev) => ({
       ...prev,
-      [questionIndex]: answer,
+      [questionIndex]: {
+        selectedAnswer: answer,
+        isCorrect:
+          quizzes[questionIndex].correct_answer === answer ? true : false,
+        answer: quizzes[questionIndex].correct_answer,
+      },
     }));
+  }
+
+  function checkAnswers() {
+    const correctAnswers = Object.values(selectedAnswers).filter(item => item.isCorrect).length
+    setCorrectAnswers(correctAnswers)
+    setIsChecked((prev) => !prev);
+  }
+
+  function handleReset() {
+    fetchData();
+    setIsChecked(false);
+    setSelectedAnswers({});
+    setCorrectAnswers(0)
   }
   return (
     <>
-      {quizzes.length !== 0 && (isHomePage ? (
-        <HomePage onClick={start} />
-      ) : (
-        <QuizPage
-          quizzes={quizzes}
-          selectedAnswers={selectedAnswers}
-          onSelect={handleSelect}
-        />
-      ))}
+      {
+        (isHomePage ? (
+          <HomePage onClick={start} />
+        ) : quizzes.length !== 0 && (
+          <QuizPage
+            quizzes={quizzes}
+            selectedAnswers={selectedAnswers}
+            onSelect={handleSelect}
+            checkAnswers={checkAnswers}
+            isChecked={isChecked}
+            handleReset={handleReset}
+            correctAnswers={correctAnswers}
+          />
+        ))}
     </>
   );
 }
--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react";
 import { decode } from 'html-entities'
+import { clsx } from 'clsx';
 import "../styles/QuizPage.css";
 
 export function QuizPage(props) {
@@ -23,15 +24,23 @@ export function QuizPage(props) {
       <div className="question-block" key={index}>
         <p className="q-text">{decode(quiz.question)}</p>
         <div className="options">
-          {(shuffledAnswers[index] || []).map((answer, i) => (
+          {(shuffledAnswers[index] || []).map((answer, i) => {
+            const className = clsx({
+                selected : props.selectedAnswers[index]?.selectedAnswer === answer,
+                right : props.isChecked && props.selectedAnswers[index]?.answer === answer  ,
+                wrong : props.isChecked && props.selectedAnswers[index]?.selectedAnswer === answer  && !props.selectedAnswers[index]?.isCorrect,
+                others : props.isChecked && props.selectedAnswers[index]?.answer !== answer
+            })
+            return(
             <button
               key={i}
-              className={`opt ${props.selectedAnswers[index] === answer ? "selected" : ""}`}
+              className={`opt ${className}`}
               onClick={() => props.onSelect(index, answer)}
+              disabled={props.isChecked ? true : false}
             >
               {decode(answer)}
             </button>
-          ))}
+          )})}
         </div>
         <hr></hr>
       </div>
@@ -47,7 +56,9 @@ export function QuizPage(props) {
         {quizzesElement}
 
         <div className="check-btn-wrap">
-          <button className="check-btn">Check answers</button>
+          {props.isChecked ? <div className="check-btn-wrap">
+          <p>You scored {props.correctAnswers} correct answers</p> 
+          <button onClick={props.handleReset}>Play again</button></div> : <button className="check-btn" onClick={props.checkAnswers}>Check answers</button>}
         </div>
       </div>
     </>
